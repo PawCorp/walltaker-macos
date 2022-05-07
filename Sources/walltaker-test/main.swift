2f58f1952d2327bcf78600be16826b04d67dc7ac
Pup Gray <pupgray@outlook.com>
2022-05-07 11:45:47 -0400

    What you might call "embettering" slightly

--- Sources/walltaker-test/main.swift
+++ Sources/walltaker-test/main.swift
@@ -1,6 +1,8 @@
 import Cocoa
 import Foundation
 
+var lastPostUrl: String = ""
+
 func setWallpaper(path: String) {
     let newWallpaperURL = URL(fileURLWithPath: path)
     if let screen = NSScreen.main {
@@ -8,8 +10,9 @@ func setWallpaper(path: String) {
     }
 }
 
-protocol Link {
-    var post_url: String { get set }
+struct Link: Codable {
+    var post_url: String?
+    var set_by: String?
 }
 
 func getLink(id: Int) {
@@ -19,33 +22,54 @@ func getLink(id: Int) {
         guard let data = data else {
             return
         }
-        let link = try? JSONSerialization.jsonObject(with: data) as! [String: Any]
+        let decoder = JSONDecoder()
+        do {
+            let link = try decoder.decode(Link.self, from: data)
+            print(link)
+            if link.post_url != nil {
+                if (link.post_url != lastPostUrl) {
+                    print("New post from " + (link.set_by ?? "anon"))
+                    lastPostUrl = link.post_url as! String
 
-        if link?["post_url"] != nil {
-            print(link?["post_url"])
+                    let directory = NSTemporaryDirectory()
+                    let fileName = NSUUID().uuidString
 
-            let directory = NSTemporaryDirectory()
-            let fileName = NSUUID().uuidString
+                    guard let fullURL = NSURL.fileURL(withPathComponents: [directory, fileName]) else {
+                        fatalError("guard failure handling has not been implemented")
+                    }
 
-            guard let fullURL = NSURL.fileURL(withPathComponents: [directory, fileName]) else {
-                fatalError("guard failure handling has not been implemented")
+                    let url = URL(string: link.post_url as! String)
+                    let data: Data? = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+                    print(data)
+
+                    try! data?.write(to: fullURL)
+                    print(fullURL.path)
+
+                    setWallpaper(path: fullURL.path)
+                } else {
+                    print("Wallpaper remains unchanged.")
+                }
             }
-
-            let url = URL(string: link?["post_url"] as! String)
-            let data: Data? = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
-            print(data)
-
-            try! data?.write(to: fullURL)
-            print(fullURL.path)
-
-            setWallpaper(path: fullURL.path)
+        } catch {
+            print(error.localizedDescription, "sdf")
         }
     }
 
     task.resume()
 }
 
-getLink(id: 1)
-while RunLoop.main.run(mode: .default, before: .distantFuture) {
-}
+print("Walltaker - MacOS Beta Client")
+print("-----------------------------")
+print("Enter your link ID:")
+if let linkId = Int(readLine()!) {
+    print("Starting link-pinging for ID " + String(linkId))
+    getLink(id: linkId)
 
+    while RunLoop.main.run(mode: .default, before: .distantFuture) {
+        getLink(id: linkId)
+        sleep(10)
+    }
+} else {
+    print("Quitting, that's not a link ID.")
+    exit(0)
+}
